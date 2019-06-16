7a0c8532b55874f7dae3a3bad0ca92b88413371e
Mads Smed Thomassen <mads.th@hotmail.com>
2019-06-16 19:17:08 +0200

    Added correct db settings

--- src/app/services/question-data.service.ts
+++ src/app/services/question-data.service.ts
@@ -9,45 +9,46 @@ export class QuestionDataService {
 
     public questions: Array<question>;
     public answers: answer[];
+    public BASE_URL: string = "http://questions-api.azurewebsites.net/api/";
     constructor(private _http: HttpClient) { }
 
     getQuestions(): Promise<question[]> {
-        return this._http.get<question[]>('http://localhost:5000/api/question/').toPromise();
+        return this._http.get<question[]>(this.BASE_URL + 'question/').toPromise();
     }
 
     getQuestion(id: number): Promise<question> {
-        return this._http.get<question>('http://localhost:5000/api/question/' + id).toPromise();
+        return this._http.get<question>(this.BASE_URL + 'question/' + id).toPromise();
     }
 
     postQuestion(question: string): Promise<question> {
         if (question != "") {
             let q = {questionText: question, questionId: undefined, answers: undefined};
-            return this._http.post<question>('http://localhost:5000/api/question/', q).toPromise();
+            return this._http.post<question>(this.BASE_URL + 'question/', q).toPromise();
         }
     }
 
     deleteQuestion(questionId: number): Promise<question> {
         if (questionId != undefined) {
-            return this._http.delete<question>('http://localhost:5000/api/question/' + questionId).toPromise();
+            return this._http.delete<question>(this.BASE_URL + 'question/' + questionId).toPromise();
         }
     }
 
     postAnswer(answer: string, questionId: number): Promise<answer> {
         if (answer != "") {
             let a = {answerText: answer, answerId: undefined, questionId: questionId};
-            return this._http.post<answer>('http://localhost:5000/api/answer/', a).toPromise();
+            return this._http.post<answer>(this.BASE_URL + 'answer/', a).toPromise();
         }
     }
 
     deleteAnswer(answerId: number) {
         if (answerId != undefined) {
             console.log("not undefined");
-            return this._http.delete<answer>('http://localhost:5000/api/answer/' + answerId)
+            return this._http.delete<answer>(this.BASE_URL + 'answer/' + answerId)
                 .subscribe(res => console.log(res));
         }
     }
 
     getAnswers(questionId: number): Promise<answer[]> {
-        return this._http.get<answer[]>('http://localhost:5000/api/question/' + questionId + '/answers').toPromise();
+        return this._http.get<answer[]>(this.BASE_URL + 'question/' + questionId + '/answers').toPromise();
     }
 }
